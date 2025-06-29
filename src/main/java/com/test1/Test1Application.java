98636df40fa72f975cb1e4ece331aa4d1b6379c0
varsha <varshabisht1120@gmail.com>
2025-06-29 22:44:05 +0530

    defect - 3456- login error - closed

--- src/main/java/com/test1/Test1Application.java
+++ src/main/java/com/test1/Test1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test1Application {
 
 	public static void main(String[] args) {
+                int x = 100;
 		SpringApplication.run(Test1Application.class, args);
 	}
 
